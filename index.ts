a8baa330f5d1521ecb83f1e7752d933eb47f68cb
Christopher Brown <io@henrian.com>
2018-02-09 18:22:37 -0600

    Remove semicolons

--- index.ts
+++ index.ts
@@ -8,39 +8,39 @@ null, false, etc., is all equivalent to having a 0 count.
 */
 export class Bag {
   //if (counts instanceof Bag) {
-  //  return counts.clone();
+  //  return counts.clone()
   //}
   //else if (elements instanceof Set) {
   //  // creating a new S from another S will create a copy
-  //  elements = elements.toArray();
+  //  elements = elements.toArray()
   //}
-  private _element_object: {[index: string]: number};
+  private _element_object: {[index: string]: number}
   constructor(elements: Array<string> = []) {
-    this._element_object = {};
+    this._element_object = {}
     for (var i = 0, element; (element = elements[i]); i++) {
-      this._element_object[element] = 1;
+      this._element_object[element] = 1
     }
   }
   // handle overloading constructor without `new` keyword
   //if (!(this instanceof Bag)) {
-  //  return new Bag(counts);
+  //  return new Bag(counts)
   //}
 
   //countOf(element) {
   //  /**
   //   * Return the number of times `element` shows up in this Bag, a.k.a., the
   //   * multiplicity of `element` */
-  //  //return this._element_object[element];
+  //  //return this._element_object[element]
   //}
   //_add(elements) {
   //  /** */
-  //  //return this._element_object[element];
+  //  //return this._element_object[element]
   //}
   //
   //static fromSet(element) {
   //  /** Return the number of times `element` shows up in this Bag, a.k.a., the
   //   multiplicity of `element` */
-  //  //return this._element_object[element];
+  //  //return this._element_object[element]
   //}
 }
 
@@ -49,15 +49,15 @@ export class Bag {
 function keysEqual(a: {[index: string]: any}, b: {[index: string]: any}) {
   for (var b_element in b) {
     if (!(b_element in a)) {
-      return false;
+      return false
     }
   }
   for (var a_element in a) {
     if (!(a_element in b)) {
-      return false;
+      return false
     }
   }
-  return true;
+  return true
 }
 
 /** new Set(elements?: string[])
@@ -70,25 +70,25 @@ the value does not matter, only their presence does.
 All elements are coerced to strings by object index notation.
 */
 export class Set {
-  private _element_object: {[index: string]: boolean};
+  private _element_object: {[index: string]: boolean}
   /** Create a new Set, optionally initializing it with an Array of strings */
   constructor(elements?: string[]) {
-    this._element_object = {};
-    this._addArray(elements);
+    this._element_object = {}
+    this._addArray(elements)
   }
   /** Clone this set, returning the copy.
    * [immutable] */
   clone() {
-    var copy = new Set([]);
+    var copy = new Set([])
     for (var element in this._element_object) {
-      copy._element_object[element] = true;
+      copy._element_object[element] = true
     }
-    return copy;
+    return copy
   }
   /** Return an unordered Array of strings representing this Set.
    * [immutable] */
   toJSON() {
-    return Object.keys(this._element_object);
+    return Object.keys(this._element_object)
   }
 
   //
@@ -98,45 +98,45 @@ export class Set {
   /** Add a single element to this set.
    * [mutable, chainable] */
   _add(element: string) {
-    this._element_object[element] = true;
-    return this;
+    this._element_object[element] = true
+    return this
   }
   /** Add multiple elements to this set.
    * [mutable, chainable] */
   _addArray(elements: string[] = []) {
     for (var i = 0, element; (element = elements[i]) !== undefined; i++) {
-      this._element_object[element] = true;
+      this._element_object[element] = true
     }
-    return this;
+    return this
   }
   /** Add all elements from another Set.
    * [mutable, chainable] */
   _addSet(other: Set) {
     for (var element in other._element_object) {
-      this._element_object[element] = true;
+      this._element_object[element] = true
     }
-    return this;
+    return this
   }
   /** Remove a single element from this set. No-op if the element doesn't exist.
    * [mutable, chainable] */
   _remove(element: string) {
-    delete this._element_object[element];
+    delete this._element_object[element]
   }
   /** Remove multiple elements from this set.
    * [mutable, chainable] */
   _removeArray(elements: string) {
     for (var i = 0, element; (element = elements[i]) !== undefined; i++) {
-      delete this._element_object[element];
+      delete this._element_object[element]
     }
-    return this;
+    return this
   }
   /** Remove all elements from another Set.
    * [mutable, chainable] */
   _removeSet(other: Set) {
     for (var element in other._element_object) {
-      delete this._element_object[element];
+      delete this._element_object[element]
     }
-    return this;
+    return this
   }
 
   //
@@ -147,36 +147,36 @@ export class Set {
    * element.
    * [immutable, chainable] */
   add(element: string) {
-    return this.clone()._add(element);
+    return this.clone()._add(element)
   }
   /** Return a new Set representing the union of this set and the given
    * elements.
    * [immutable, chainable] */
   addArray(elements: string[]) {
-    return this.clone()._addArray(elements);
+    return this.clone()._addArray(elements)
   }
   /** Return a new Set representing the union of this set and another set.
    * [immutable, chainable] */
   addSet(other: Set) {
-    return this.clone()._addSet(other);
+    return this.clone()._addSet(other)
   }
   /** Return a new Set representing the subtraction of the given element from
    * this Set.
    * [immutable, chainable] */
   remove(element: string) {
-    return this.clone()._remove(element);
+    return this.clone()._remove(element)
   }
   /** Return a new Set representing the subtraction of the given elements from
    * this Set.
    * [immutable, chainable] */
   removeArray(elements: string) {
-    return this.clone()._removeArray(elements);
+    return this.clone()._removeArray(elements)
   }
   /** Return a new Set representing the subtraction of the given Set from
    * this Set.
    * [immutable, chainable] */
   removeSet(other: Set) {
-    return this.clone()._removeSet(other);
+    return this.clone()._removeSet(other)
   }
 
   //
@@ -190,21 +190,21 @@ export class Set {
    * new Set(['a', 'b', 'z']).equal(new Set(['a', 'b'])) == false
    * [immutable] */
   equals(other: Set) {
-    return keysEqual(this._element_object, other._element_object);
+    return keysEqual(this._element_object, other._element_object)
   }
   /** Check if the given element is contained in this set. */
   contains(element: string) {
-    return element in this._element_object;
+    return element in this._element_object
   }
   /** Simply . */
   get size() {
     // TODO: use an actual property and update it whenever mutating the
     // underlying _element_object
-    var count = 0;
+    var count = 0
     for (var element in this._element_object) {
-      count++;
+      count++
     }
-    return count;
+    return count
   }
 
   //
@@ -214,63 +214,63 @@ export class Set {
   /** Compare an Array of Sets, returning true if they're all equal.
    */
   static equal(sets: Set[]) {
-    if (sets.length == 0) return undefined; // undefined
-    if (sets.length == 1) return true; // trivially true
+    if (sets.length == 0) return undefined // undefined
+    if (sets.length == 1) return true // trivially true
     // return on the first mismatch
-    var prototype_set = sets[0];
+    var prototype_set = sets[0]
     // use a for loop to allow immediate return
     for (var i = 1, other; (other = sets[i]) !== undefined; i++) {
-      var prototype_other_equal = keysEqual(prototype_set._element_object, other._element_object);
+      var prototype_other_equal = keysEqual(prototype_set._element_object, other._element_object)
       if (!prototype_other_equal) {
-        return false;
+        return false
       }
     }
-    return true;
+    return true
   }
   /** Return true if all the given Sets contain the given element.
    * Returns false on the first mismatch. */
   static contain(sets: Set[], element: string) {
     for (var i = 0, set; (set = sets[i]) !== undefined; i++) {
       if (!(element in set._element_object)) {
-        return false;
+        return false
       }
     }
-    return true;
+    return true
   }
   /** Return a new Set that contains all the elements from the given Sets.
    * [immutable] */
   static union(sets: Set[]) {
-    if (sets.length == 0) return undefined; // undefined
-    if (sets.length == 1) return sets[0].clone(); // trivial
+    if (sets.length == 0) return undefined // undefined
+    if (sets.length == 1) return sets[0].clone() // trivial
 
-    var union_set = new Set([]);
+    var union_set = new Set([])
     for (var i = 0, set; (set = sets[i]) !== undefined; i++) {
-      union_set._addSet(set);
+      union_set._addSet(set)
     }
-    return union_set;
+    return union_set
   }
 
   /** Return a new Set representing the intersection of all given Sets.
    * [immutable] */
   static intersection(sets: Set[]) {
-    if (sets.length == 0) return undefined; // undefined
-    if (sets.length == 1) return sets[0].clone(); // trivial
+    if (sets.length == 0) return undefined // undefined
+    if (sets.length == 1) return sets[0].clone() // trivial
 
     // reorder sets from smallest to largest
     sets = sets.sort(function(a, b) {
-      return a.size - b.size;
-    });
-    var prototype_set = sets[0];
-    var other_sets = sets.slice(1);
+      return a.size - b.size
+    })
+    var prototype_set = sets[0]
+    var other_sets = sets.slice(1)
 
-    var intersection_set = new Set([]);
+    var intersection_set = new Set([])
     for (var element in prototype_set._element_object) {
-      var other_sets_contain_element = Set.contain(other_sets, element);
+      var other_sets_contain_element = Set.contain(other_sets, element)
       if (other_sets_contain_element) {
-        intersection_set._element_object[element] = true;
+        intersection_set._element_object[element] = true
       }
     }
-    return intersection_set;
+    return intersection_set
   }
 }
 
@@ -290,19 +290,19 @@ export class Stack<T> {
   Returns the contents of the stack, from bottom to top.
   */
   getElements(): T[] {
-    return this.elements;
+    return this.elements
   }
   /**
   Return the size (length) of the stack.
   */
   get size(): number {
-    return this.elements.length;
+    return this.elements.length
   }
   /**
   Add a new element to the top of the stack and return the new size of the stack.
   */
   push(element: T): number {
-    return this.elements.push(element);
+    return this.elements.push(element)
   }
   /**
   Remove the element at the top of the stack and return it.
@@ -310,7 +310,7 @@ export class Stack<T> {
   Returns undefined if the stack is empty.
   */
   pop(): T {
-    return this.elements.pop();
+    return this.elements.pop()
   }
   /**
   Retrieve the bottom element of the stack.
@@ -318,7 +318,7 @@ export class Stack<T> {
   Returns undefined if the stack is empty.
   */
   get bottom(): T {
-    return this.elements[0];
+    return this.elements[0]
   }
   /**
   Replace the bottom element of the stack.
@@ -326,7 +326,7 @@ export class Stack<T> {
   Has the same effect as `Stack#push(element)` if the stack is empty.
   */
   set bottom(element: T) {
-    this.elements[0] = element;
+    this.elements[0] = element
   }
   /**
   Retrieve the top element of the stack.
@@ -334,7 +334,7 @@ export class Stack<T> {
   Returns undefined if the stack is empty.
   */
   get top(): T {
-    return this.elements[this.elements.length - 1];
+    return this.elements[this.elements.length - 1]
   }
   /**
   Replace the top element of the stack.
@@ -342,7 +342,7 @@ export class Stack<T> {
   Has the same effect as `Stack#push(element)` if the stack is empty.
   */
   set top(element: T) {
-    var index = Math.max(this.elements.length - 1, 0);
-    this.elements[index] = element;
+    var index = Math.max(this.elements.length - 1, 0)
+    this.elements[index] = element
   }
 }
--- tests/set.js
+++ tests/set.js
@@ -1,77 +1,77 @@
-import assert from 'assert';
-import {describe, it} from 'mocha';
+import assert from 'assert'
+import {describe, it} from 'mocha'
 
-import {Set} from '..';
+import {Set} from '..'
 
 describe('{m,o,d,e,l}', () => {
-  var model = new Set(['m', 'o', 'd', 'e', 'l']);
+  var model = new Set(['m', 'o', 'd', 'e', 'l'])
   it('should equal {l,o,m,e,d}', () => {
-    var equivalent = new Set(['l', 'o', 'm', 'e', 'd']);
-    assert(model.equals(equivalent));
-  });
+    var equivalent = new Set(['l', 'o', 'm', 'e', 'd'])
+    assert(model.equals(equivalent))
+  })
   it('should not equal {e,l,s,e}', () => {
-    var inequivalent = new Set(['e', 'l', 's', 'e']);
-    assert(!model.equals(inequivalent));
-  });
+    var inequivalent = new Set(['e', 'l', 's', 'e'])
+    assert(!model.equals(inequivalent))
+  })
   it('should not equal {super,m,o,d,e,l} (a superset)', () => {
-    var superset = new Set(['super', 'm', 'o', 'd', 'e', 'l']);
-    assert(!model.equals(superset));
-  });
+    var superset = new Set(['super', 'm', 'o', 'd', 'e', 'l'])
+    assert(!model.equals(superset))
+  })
   it('should not equal {m,o,d} (a subset)', () => {
-    var subset = new Set(['m', 'o', 'd']);
-    assert(!model.equals(subset));
-  });
-});
+    var subset = new Set(['m', 'o', 'd'])
+    assert(!model.equals(subset))
+  })
+})
 
 describe('{a,b,c}._add(d)', () => {
-  var abc = new Set(['a', 'b', 'c']);
+  var abc = new Set(['a', 'b', 'c'])
   it('should modify and return the original Set', () => {
-    var abcd = abc._add('d');
-    assert(abc.equals(new Set(['a', 'b', 'c', 'd'])));
-    assert(abcd.equals(abc));
-    assert.equal(abcd, abc);
-  });
-});
+    var abcd = abc._add('d')
+    assert(abc.equals(new Set(['a', 'b', 'c', 'd'])))
+    assert(abcd.equals(abc))
+    assert.equal(abcd, abc)
+  })
+})
 
 describe('{a,b,c}.add(d)', () => {
-  var abc = new Set(['a', 'b', 'c']);
+  var abc = new Set(['a', 'b', 'c'])
   it('should return a new Set and not modify the original Set', () => {
-    var abcd = abc.add('d');
-    assert(abc.equals(new Set(['a', 'b', 'c'])));
-    assert(abcd.equals(new Set(['a', 'b', 'c', 'd'])));
-    assert.notEqual(abcd, abc);
-  });
-});
+    var abcd = abc.add('d')
+    assert(abc.equals(new Set(['a', 'b', 'c'])))
+    assert(abcd.equals(new Set(['a', 'b', 'c', 'd'])))
+    assert.notEqual(abcd, abc)
+  })
+})
 
 describe('Set.intersection([{a,b,c}, {b,c,d}, {b,d,e}])', () => {
-  var abc = new Set(['a', 'b', 'c']);
-  var bcd = new Set([     'b', 'c', 'd']);
-  var bde = new Set([     'b',      'd', 'e']);
+  var abc = new Set(['a', 'b', 'c'])
+  var bcd = new Set([     'b', 'c', 'd'])
+  var bde = new Set([     'b',      'd', 'e'])
   it('should return {b}', () => {
-    var intersection = Set.intersection([abc, bcd, bde]);
-    assert(intersection.equals(new Set(['b'])));
-  });
-});
+    var intersection = Set.intersection([abc, bcd, bde])
+    assert(intersection.equals(new Set(['b'])))
+  })
+})
 
 describe('Set.contain([{a,b,c}, {b,c,d}], ?)', () => {
-  var abc = new Set(['a', 'b', 'c']);
-  var bcd = new Set([     'b', 'c', 'd']);
+  var abc = new Set(['a', 'b', 'c'])
+  var bcd = new Set([     'b', 'c', 'd'])
   it('should return true for b and c', () => {
-    assert(Set.contain([abc, bcd], 'b'));
-    assert(Set.contain([abc, bcd], 'c'));
-  });
+    assert(Set.contain([abc, bcd], 'b'))
+    assert(Set.contain([abc, bcd], 'c'))
+  })
   it('should return false for a and d', () => {
-    assert(!Set.contain([abc, bcd], 'a'));
-    assert(!Set.contain([abc, bcd], 'd'));
-  });
-});
+    assert(!Set.contain([abc, bcd], 'a'))
+    assert(!Set.contain([abc, bcd], 'd'))
+  })
+})
 
 describe('Set.union([{a,b,c}, {b,c,d}, {b,d,e}])', () => {
-  var abc = new Set(['a', 'b', 'c']);
-  var bcd = new Set([     'b', 'c', 'd']);
-  var bde = new Set([     'b',      'd', 'e']);
+  var abc = new Set(['a', 'b', 'c'])
+  var bcd = new Set([     'b', 'c', 'd'])
+  var bde = new Set([     'b',      'd', 'e'])
   it('should return {a,b,c,d,e}', () => {
-    var union = Set.union([abc, bcd, bde]);
-    assert(union.equals(new Set(['a', 'b', 'c', 'd', 'e'])));
-  });
-});
+    var union = Set.union([abc, bcd, bde])
+    assert(union.equals(new Set(['a', 'b', 'c', 'd', 'e'])))
+  })
+})
